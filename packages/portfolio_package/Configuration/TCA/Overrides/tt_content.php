b71ca5dd938bd7f3f115f8061070de92fc166ba4
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2025-06-02 15:16:14 +0200

    edit tt_content.php 'Hide the builtin content elements' + edit setup.typoscript file + edit the ext_localconf.php 'set the path of NewCotnentElement.tsconfig' + create new dirs/subdirs to set tsconfig file NewContentElement.tsconfig

--- packages/portfolio_package/Configuration/TCA/Overrides/tt_content.php
+++ packages/portfolio_package/Configuration/TCA/Overrides/tt_content.php
@@ -6,43 +6,54 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
-// Define an array with your custom content elements
+// for the wizard 
+ExtensionManagementUtility::addPageTSConfig(
+    '@import "EXT:portfolio_package/Configuration/PageTS/Mod/Wizards/NewContentElement.tsconfig"'
+);
+
+// Define your custom content elements
 $elements = [
     [
         'label'  => 'Header',
         'value'  => 'portfolio_header',
-        'icon'   => 'EXT:portfolio_package/Resources/Public/Icons/ce_header.svg',
+        'icon'   => 'actions-star',
         'fields' => '--palette--;;headers, image,',
+        'group'  => 'portfolio_package',
     ],
     [
         'label'  => 'Hero Section',
         'value'  => 'portfolio_hero',
-        'icon'   => 'EXT:portfolio_package/Resources/Public/Icons/ce_hero.svg',
+        'icon'   => 'actions-user',
         'fields' => '--palette--;;headers, bodytext, image, link,',
+        'group'  => 'portfolio_package',
     ],
     [
         'label'  => 'About Block',
         'value'  => 'portfolio_aboutblock',
-        'icon'   => 'EXT:portfolio_package/Resources/Public/Icons/ce_aboutblock.svg',
+        'icon'   => 'actions-briefcase',
         'fields' => '--palette--;;headers, bodytext,',
+        'group'  => 'portfolio_package',
     ],
     [
         'label'  => 'Project Item',
         'value'  => 'portfolio_projectitem',
-        'icon'   => 'EXT:portfolio_package/Resources/Public/Icons/ce_projectitem.svg',
+        'icon'   => 'actions-briefcase',
         'fields' => '--palette--;;headers, bodytext, image, link,',
+        'group'  => 'portfolio_package',
     ],
     [
         'label'  => 'Contact Block',
         'value'  => 'portfolio_contactblock',
-        'icon'   => 'EXT:portfolio_package/Resources/Public/Icons/ce_contactblock.svg',
+        'icon'   => 'actions-contact',
         'fields' => '--palette--;;headers, bodytext,',
+        'group'  => 'portfolio_package',
     ],
     [
         'label'  => 'Footer',
         'value'  => 'portfolio_footer',
-        'icon'   => 'EXT:portfolio_package/Resources/Public/Icons/ce_footer.svg',
+        'icon'   => 'actions-link',
         'fields' => '--palette--;;headers,footer_github,footer_linkedin,footer_email,',
+        'group'  => 'portfolio_package',
     ],
 ];
 
@@ -71,18 +82,18 @@ ExtensionManagementUtility::addTCAcolumns('tt_content', [
     ],
 ]);
 
-// Loop through and add each element to the tt_content CType dropdown
+// Register the elements in CType dropdown and assign to the group
 foreach ($elements as $element) {
     ExtensionManagementUtility::addTcaSelectItem(
-        'tt_content', // The table you want to add the item to.
-        'CType',     // The field (content type) to modify.
+        'tt_content',
+        'CType',
         [
-            $element['label'],  // The label shown in the backend.
-            $element['value'],  // The internal value.
-            $element['icon'] ?? null, // Your custom icon.
-            'default'
+            $element['label'],
+            $element['value'],
+            $element['icon'] ?? null,
+            $element['group'] ?? null,
         ],
-        'textmedia', // The item to position your custom element after.
+        'textmedia',
         'after'
     );
 
--- packages/portfolio_package/ext_localconf.php
+++ packages/portfolio_package/ext_localconf.php
@@ -1,1 +1,7 @@
 <?php
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '@import "EXT:portfolio_package/Configuration/PageTS/Mod/Wizards/NewContentElement.tsconfig"'
+);
+
